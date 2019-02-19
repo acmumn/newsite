3c4b214df41740bfaa5dc390cc1a766b057568dc
Michael Zhang <iptq@protonmail.com>
2019-02-19 12:26:03 -0600

    ...

--- acmumn/__init__.py
+++ acmumn/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 
-from acmumn.globals import db, login_manager
+from acmumn.globals import assets, db, login_manager
 
 app = Flask(__name__)
 
+assets.init_app(app)
 db.init_app(app)
 login_manager.init_app(app)
+
+import acmumn.api as api
+app.register_blueprint(api.blueprint, url_prefix="/api")
 
 import acmumn.views as views
 app.register_blueprint(views.blueprint)
--- acmumn/globals.py
+++ acmumn/globals.py
@@ -1,5 +1,7 @@
-from flask_login import LoginManager
-from flask_sqlalchemy import SQLAlchemy
+import flask_assets
+import flask_login
+import flask_sqlalchemy
 
-db = SQLAlchemy()
-login_manager = LoginManager()
+assets = flask_assets.Environment()
+db = flask_sqlalchemy.SQLAlchemy()
+login_manager = flask_login.LoginManager()
